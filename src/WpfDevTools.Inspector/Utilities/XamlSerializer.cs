79f44454ee70b654722b0a52a83ff0ca0a6bef96
evanlau1798 <sl30608@gmail.com>
2026-03-04 05:46:42 +0800

    feat: add XamlSerializer utility

---
+++ src/WpfDevTools.Inspector/Utilities/XamlSerializer.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace WpfDevTools.Inspector.Utilities;
+
+/// <summary>
+/// Serializes WPF elements to XAML
+/// </summary>
+public class XamlSerializer
+{
+    public string SerializeToXaml(DependencyObject element)
+    {
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element));
+        }
+
+        try
+        {
+            var sb = new StringBuilder();
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "  ",
+                OmitXmlDeclaration = true
+            };
+
+            using (var writer = XmlWriter.Create(sb, settings))
+            {
+                XamlWriter.Save(element, writer);
+            }
+
+            return sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            return $"<!-- Failed to serialize: {ex.Message} -->";
+        }
+    }
+
+    public string SerializeToXaml(DependencyObject element, bool includeDefaults)
+    {
+        // TODO: Implement option to include/exclude default values
+        return SerializeToXaml(element);
+    }
+}
